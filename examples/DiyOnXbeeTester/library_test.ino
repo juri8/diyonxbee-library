57742a71881a844445fe3a50f714e32c70ec64c5
Jürgen Richtsfeld <juergen.richtsfeld@gmail.com>
2016-03-12 09:59:30 +0100

    Change library usage in sample wich reduces binary size from 8122 to 7532 bytes for a Atmega 328

--- examples/DiyOnXbeeTester/library_test.ino
+++ examples/DiyOnXbeeTester/library_test.ino
@@ -22,19 +22,24 @@ void printDhtTemperature(Stream& stream) {
 void printDhtHumidity(Stream& stream) {
   stream.println(dht.readHumidity());
 }
+AnalogSensor brightness("Brightness", FOTOPIN, 5000);
+GenericSensor temperature ("Temperature", 3000, &printDhtTemperature);
+GenericSensor humidity("Humidity", 3000, &printDhtHumidity);
+Dimmer dimmer("Dimmer", 10);
+RGB stripe("Stripe", 7, 8, 9);
 
 void setup() {
   dht.begin();
   Serial.begin(115200);
 
   int i = -1;
-  sensors.setSensor(++i, new AnalogSensor("Brightness", FOTOPIN, 5000));
-  sensors.setSensor(++i, new GenericSensor("Temperature", 3000, &printDhtTemperature));
-  sensors.setSensor(++i, new GenericSensor("Humidity", 3000, &printDhtHumidity));
+  sensors.setSensor(++i, &brightness);
+  sensors.setSensor(++i, &temperature);
+  sensors.setSensor(++i, &humidity);
 
   i = -1;
-  sensors.setActor(++i, new Dimmer("Dimmer", 10));
-  sensors.setActor(++i, new RGB("Stripe", 7, 8, 9));
+  sensors.setActor(++i, &dimmer);
+  sensors.setActor(++i, &stripe);
 }
 
 void loop() {
